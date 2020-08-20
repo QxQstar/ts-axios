a0c846d47c2d9ce5d75007a87672de1a111352fa
何遇 <1395294694@qq.com>
2020-08-20 17:42:54 +0800

    feat: 转换 request data 的 demo

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -1,61 +1,76 @@
 import axios from '../../src/index'
 
+// axios({
+//   url:'/base/get',
+//   params:{
+//     a:1,
+//     b:2
+//   }
+// })
+//
+// axios({
+//   url:'/base/get',
+//   params:{
+//     a:1,
+//     b:null
+//   }
+// })
+//
+// axios({
+//   url:'/base/get',
+//   params:{
+//     a:[1,2]
+//   }
+// })
+//
+// axios({
+//   url:'/base/get',
+//   params:{
+//     a:{
+//       name:'22'
+//     }
+//   }
+// })
+//
+// axios({
+//   url:'/base/get',
+//   params:{
+//     a:'@:[33]'
+//   }
+// })
+//
+// axios({
+//   url:'/base/get#hass',
+//   params:{
+//     a:'@:[33]'
+//   }
+// })
+//
+// axios({
+//   url:'/base/get?b=3',
+//   params:{
+//     a:'55'
+//   }
+// })
+//
+// axios({
+//   url:'/base/get?b=3',
+//   params:{
+//     date:new Date()
+//   }
+// })
+
 axios({
-  url:'/base/get',
-  params:{
-    a:1,
-    b:2
+  url:'/base/post',
+  method: 'post',
+  data:{
+    a:'a',
+    b:'b'
   }
 })
 
 axios({
-  url:'/base/get',
-  params:{
-    a:1,
-    b:null
-  }
-})
-
-axios({
-  url:'/base/get',
-  params:{
-    a:[1,2]
-  }
-})
-
-axios({
-  url:'/base/get',
-  params:{
-    a:{
-      name:'22'
-    }
-  }
-})
-
-axios({
-  url:'/base/get',
-  params:{
-    a:'@:[33]'
-  }
-})
-
-axios({
-  url:'/base/get#hass',
-  params:{
-    a:'@:[33]'
-  }
-})
-
-axios({
-  url:'/base/get?b=3',
-  params:{
-    a:'55'
-  }
-})
-
-axios({
-  url:'/base/get?b=3',
-  params:{
-    date:new Date()
-  }
+  url:'/base/buffer',
+  method: 'post',
+  data:new Int32Array([100,200])
 })
