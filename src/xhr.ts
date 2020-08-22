90bbf882bc8d4208d428ea5d1a8b7919543cc5f6
何遇 <1395294694@qq.com>
2020-08-22 16:28:12 +0800

    feat: axios 返回 promise

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,11 +1,36 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 
-export default function xhr(config: AxiosRequestConfig): void {
-  const { url, method = 'get', data = null, headers } = config
-  const xhr = new XMLHttpRequest()
-  xhr.open(method.toUpperCase(), url, true)
-  Object.keys(headers).forEach(name => {
-    xhr.setRequestHeader(name, headers[name])
+export default function xhr(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise((resolve, reject) => {
+    const { url, method = 'get', data = null, headers, responseType } = config
+    const xhr = new XMLHttpRequest()
+    if (responseType) {
+      xhr.responseType = responseType
+    }
+    xhr.open(method.toUpperCase(), url, true)
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== 4) {
+        return
+      }
+      const responseData = responseType && responseType !== 'text' ? xhr.response : xhr.responseText
+      const response: AxiosResponse = {
+        status: xhr.status,
+        statusText: xhr.statusText,
+        request: xhr,
+        data: responseData,
+        headers: xhr.getAllResponseHeaders(),
+        config
+      }
+
+      resolve(response)
+    }
+    Object.keys(headers).forEach(name => {
+      if (data === null && name.toLowerCase() === 'content-type') {
+        delete headers[name]
+      } else {
+        xhr.setRequestHeader(name, headers[name])
+      }
+    })
+    xhr.send(data)
   })
-  xhr.send(data)
 }
